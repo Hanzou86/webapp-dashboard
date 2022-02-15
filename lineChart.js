d8b764bd74e0a91322080d3a4584ab3d16cfa1a6
James Albert <James.Albert1986@outlook.com>
2022-02-15 15:28:59 -0500

    update

--- lineChart.js
+++ lineChart.js
@@ -1,10 +1,15 @@
 const lineChartLabels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
+    '16-22',
+    '23-29',
+    '30-5',
+    '6-12',
+    '13-19',
+    '20-26',
+    '27-3',
+    '4-10',
+    '11-17',
+    '18-24',
+    '25-31',
 ];
 
 
@@ -13,9 +18,11 @@ const lineChartData = {
     labels: lineChartLabels,
     datasets: [{
     label: 'My First dataset',
+    tension: .4,
+    borderWidth: 2,
     backgroundColor: 'rgb(255, 99, 132)',
     borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1900, 2250, 1500, 2500],
     }]
 };
 
