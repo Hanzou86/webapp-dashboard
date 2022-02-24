5af7582dbb0777b0cfbfaf63f46449158c4c57af
James Albert <James.Albert1986@outlook.com>
2022-02-24 16:39:47 -0500

    Update
    -Completed all three layouts
    -Completed toggle buttons
    -Need to review instructions and finish requirements for exceeds grading

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const alert = document.querySelector('.alert');
-const alertClose = document.querySelector('.alert-close')
+const alertClose = document.querySelector('.alert-close');
+const onOff = document.querySelector('.on-off')
+const switchInput = document.querySelector('#notifications')
+
 
 function closeElement(btn) {
     btn.parentNode.style.display = 'none';
@@ -7,4 +10,13 @@ function closeElement(btn) {
 
 alertClose.addEventListener( 'click', (event) => {
     closeElement(alertClose);
+})
+
+switchInput.addEventListener('click', (event) => {
+    if (onOff.textContent === 'ON') {
+        onOff.textContent = 'OFF';
+    } else {
+        onOff.textContent = 'ON';
+    }
+
 })
